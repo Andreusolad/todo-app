791863502a3644f79f8570326f97b2bf18417ec2
andreusola <140717026+andreusola@users.noreply.github.com>
2025-07-10 02:22:54 +0200

    Implement the Add button

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
-filepath='files/todos.txt'
-
-def get_todos(filepath):
+FILEPATH='files/todos.txt'
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
@@ -9,7 +8,7 @@ def get_todos(filepath):
     return todos_local
 
 
-def write_todos(todos_arg, filepath):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -3,15 +3,26 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do") #creats a label on window, has to be a string
-input_box = sg.InputText(tooltip="Enter todo") #és el lloc on escrivim, tooltip és el missatge que surt si passem el cursor per sobre
+input_box = sg.InputText(tooltip="Enter todo", key="todo") #és el lloc on escrivim, tooltip és el missatge que surt si passem el cursor per sobre
 add_button = sg.Button("Add")
 
 
-window = sg.Window('My To_Do App', layout=[[label], [input_box, add_button]]) #title of the window and layout
+window = sg.Window('My To_Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica',15)) #title of the window and layout
 #fiquem doble [[ ]] perquè si està dins de dos brackets fica la box al costat del label
 
+while True:
+    event, values = window.read()
+    print(event) #event és add
+    print(values) #values és el que afegeixes i que és de la categoria de to do
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n" #perquè values representa un diccionari on 'to do' és el nostre to do
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 
-
-window.read()
-print("Window has been closed")
 window.close()
